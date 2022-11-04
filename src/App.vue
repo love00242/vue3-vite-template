9d6c527660461a25222a901101ab55d8b966d236
怡瑄 <a6687225@gmail.com>
2022-11-04 16:34:39 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router';
+import HelloWorld from './components/HelloWorld.vue';
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+      <h2 class="text-red-500 text-xl">YA</h2>
+      <img src="images/test.png" alt="">
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+      </nav>
+    </div>
+  </header>
+  <RouterView />
+</template>
+
+<style scoped>
+header {
+  @apply
+  text-red-500
+  max-h-screen
+  leading-normal;
+}
+</style>
