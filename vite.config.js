41a8f6ae1c5016c59ae7a2377c20cbdbf2ede5ef
怡瑄 <a6687225@gmail.com>
2022-11-07 18:32:30 +0800

    vite rollup-plugin-copy
    vite rollup-plugin-copy

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,30 @@
-import { fileURLToPath, URL } from 'node:url';
-
+// import { fileURLToPath, URL } from 'node:url';
+const { resolve } = require('path');
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import copy from 'rollup-plugin-copy';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue({
-    // This is needed, or else Vite will try to find image paths (which it wont be able to find because this will be called on the web, not directly)
-    // For example <img src="/images/logo.png"> will not work without the code below
-    template: {
-      transformAssetUrls: {
-        includeAbsolute: false,
+  plugins: [
+    vue({
+      // This is needed, or else Vite will try to find image paths (which it wont be able to find because this will be called on the web, not directly)
+      // For example <img src="/images/logo.png"> will not work without the code below
+      template: {
+        transformAssetUrls: {
+          includeAbsolute: false,
+        },
       },
-    },
-  }
-  )],
+    }),
+    copy({
+      targets: [
+        { src: './src/assets/images/**/*', dest: 'dist/images' }
+      ],
+      hook: 'writeBundle'
+    })],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@': resolve(__dirname, './src'),
       '/images': 'src/assets/images',
     },
   },
